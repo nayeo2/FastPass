c07796afa095ed695bb1820ca520f2dcc375d77f
Nayoung Lee <nayeo2@naver.com>
2026-03-30 15:09:02 +0900

    ec2 서버 생성

--- infra/environments/dev/main.tf
+++ infra/environments/dev/main.tf
@@ -2,3 +2,12 @@ locals {
   name_prefix = "${var.project_name}-${var.environment}"
 }
 
+resource "aws_instance" "fastpass_server" {
+  ami           = "ami-0c9c942bd7bf113a2" # 서울 리전 Ubuntu 22.04 (변경 가능)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-server"
+  }
+}
+
